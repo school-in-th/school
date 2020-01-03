e058ebc21db8d308b962e80db04031418c887261
KidsDev <kidsdev@gmail.com>
2020-01-03 23:49:20 +0700

    add Package: RestFramework, drf-yasg

--- school/urls.py
+++ school/urls.py
@@ -2,8 +2,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+urlpatterns_api = [
+
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="School API",
+        default_version='v1'
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    patterns=urlpatterns_api
+)
 
 urlpatterns = [
+    path('api/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
+
     path('admin/', admin.site.urls),
 ]
 
